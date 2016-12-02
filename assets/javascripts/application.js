7cb436c2c84acf5c39fccee9074c2fef89cc4b10
jefvk <jef.vankerckhoven@student.ap.be>
2016-12-02 18:17:54 +0100

    fixed the onclick slider popup windows

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,5 +1,5 @@
 function main() {
-  var elem = document.querySelector('.main-carousel');
+  var elem = document.querySelector('.carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
@@ -8,11 +8,45 @@ var flkty = new Flickity( elem, {
 
 // element argument can be a selector string
 //   for an individual element
-var flkty = new Flickity( '.main-carousel', {
+var flkty = new Flickity( '.carousel', {
   // options
 });
 }
 
+function click () {
+
+  var cellen = document.getElementsByClassName("carousel-cell");
+  var i = 0;
+
+  while (i < cellen.length) {
+
+        cellen[i].onclick = function (){
+
+            var id = this.getAttribute("data-id");
+            console.log(id);
+
+             var uitleg = document.getElementsByName('uitleg');
+             var ii = 0;
+
+             while (ii < uitleg.length) {
+
+               uitleg[ii].className = "pure-u-1 frontpage uitleg invisible"
+
+              if (id === uitleg[ii].getAttribute("data-id")){
+                uitleg[ii].className = "pure-u-1 frontpage uitleg";
+              }
+
+              ii++;
+             }
+        }
+  i++;
+  }
+
+}
+
+
+
 window.onload = function() {
        main();
+       click();
 }
