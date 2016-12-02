4382b30e8bd27544ed33b16e58f678a000c118ea
jefvk <jef.vankerckhoven@student.ap.be>
2016-12-02 12:07:14 +0100

    hopelijk wertk het nu eens wel

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,5 +1,16 @@
 function main() {
-          // hier komt je code
+  var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
 }
 
 window.onload = function() {
